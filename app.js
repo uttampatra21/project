ca71a0128b4df847afe1f82c85bf5b408a2e6aa4
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-17 12:21:45 +0530

    0

---
+++ app.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.element', {
+    strings: ["After Successful Transition Your Acctiviton Link Will Be Updated."],
+    typeSpeed: 80,
+    backSpeed: 30,
+    loop: true,
+  });
