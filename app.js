fe85b707638187617db01cdbe373af77f78d30b9
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-18 14:52:24 +0530

    0

--- app.js
+++ app.js
@@ -10,10 +10,29 @@ var typed = new Typed('.element', {
     location.reload();
   })
 //! =========================================
-
+const alertBox = document.querySelector('.payment__alert');
+const close = document.querySelectorAll('.services__model-close');
 const partner = document.querySelectorAll('.partner_info');
-partner.forEach((i)=>{
-  i.addEventListener('click',()=>{
-  
-  })
+
+
+// const model = (alertClick) =>{
+//   alertBox[alertClick].classList.add('active');
+// }
+
+// modelBtn.forEach((mb, i)=>{
+//   mb.addEventListener('click', () =>{
+//       model(i);
+//   });
+// });
+
+close.forEach((i)=>{
+  i.addEventListener('click', () =>{
+    alertBox.forEach((mv)=>{
+          mv.classList.remove('active');
+      });
+  }); 
 });
+
+// ! =================================================================
+
+
