990bdb5cd9f3032e6e54d011cd5d53f6102c2d0a
uttam kumar patra <dev.uttampatra@gmail.com>
2023-02-19 20:24:03 +0530

    m

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var typed = new Typed('.element', {
-    strings: ["After Successful Transition Your Acctiviton Link Will Be Updated."],
-    typeSpeed: 80,
-    backSpeed: 30,
-    loop: true,
-  });
+// var typed = new Typed('.element', {
+//     strings: ["After Successful Transition Your Acctiviton Link Will Be Updated."],
+//     typeSpeed: 80,
+//     backSpeed: 30,
+//     loop: true,
+//   });
 
 // ! =========================================
+  const main = document.querySelector('.payment__alert');
+  const boxs = document.querySelectorAll('.info');
+  const close = document.querySelector('.services__model-close');
 
-
-
-//! =========================================
-
-const relode = document.querySelectorAll('#relode');
-relode.addEventListener('click',()=>{
-  alert("Please Join Membership");
-});
+  boxs.forEach((i)=>{
+    i.addEventListener('click',()=>{
+      alert("Active your account First");
+    })
+  })
